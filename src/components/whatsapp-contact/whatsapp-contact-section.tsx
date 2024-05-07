637850e36f535a57be1c83aecbf77895a047ea23
ACR1209 <andrescoronel1209@gmail.com>
2024-05-06 20:11:10 -0500

    mobile design of whatsapp contact section

--- src/components/whatsapp-contact/whatsapp-contact-section.tsx
+++ src/components/whatsapp-contact/whatsapp-contact-section.tsx
@@ -13,15 +13,15 @@ function WhatsappContactSection() {
           />
 
         <h1 className="text-4xl font-bold text-center my-3">¿Tienes preguntas?<br />Escribenos por WhatsApp</h1>
-        <div className='flex flex-row justify-center gap-10'>
+        <div className='flex flex-col mt-5 lg:flex-row justify-center gap-10'>
             <ContactNumber number='098 773 4581' place='Quito'/>
             <ContactNumber number='096 252 0001' place='Guayaquil'/>
         </div>
     
-        <div className='w-1/3 h-1/5 absolute top-0 right-0 expand-animation bg-primary'/>
-        <div className='w-1/6 h-1/5 absolute top-[20%] expand-animation right-0 bg-secondaryLight'/>
-        <div className='w-1/3 h-1/5 absolute bottom-0 expand-animation-2 left-0 bg-secondary'/>
-        <div className='w-1/6 h-1/5 absolute bottom-[20%] expand-animation left-0 bg-primaryLight'/>
+        <div className='w-1/3 h-1/5 lg:block hidden absolute top-0 right-0 expand-animation bg-primary'/>
+        <div className='w-1/6 h-1/5 lg:block hidden absolute top-[20%] expand-animation right-0 bg-secondaryLight'/>
+        <div className='w-1/3 h-1/5 lg:block hidden absolute bottom-0 expand-animation-2 left-0 bg-secondary'/>
+        <div className='w-1/6 h-1/5 lg:block hidden absolute bottom-[20%] expand-animation left-0 bg-primaryLight'/>
     </section>
   )
 }
