f45885ed6158b1a17f50aef0a8f0f17b22328501
ACR1209 <andrescoronel1209@gmail.com>
2024-05-26 19:26:47 -0500

    send email

--- src/actions/send-mail.tsx
+++ src/actions/send-mail.tsx
@@ -1,27 +1,29 @@
 "use server";
-import mailgun from "mailgun-js";
+import nodemailer from 'nodemailer';
 
-const mailgunClient = mailgun({
-  apiKey: process.env.MAILGUN_API_KEY!,
-  domain: process.env.MAILGUN_DOMAIN!,
-});
 
 export async function sendEmail({ subject, message }: {subject: string, message: string}) {
-  const emailData = {
-    to: process.env.EMAIL_TO!,
-    from: "XtH2Z@example.com",
-    subject,
-    text: message,
-  };
-
-  console.log("Sending email:", emailData);
-
   try {
-    const result = await mailgunClient.messages().send(emailData);
-    console.log("Email sent successfully!");
-    return result;
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.mailgun.org', // replace with your SMTP server
+      port: 587, // replace with your SMTP port
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.MAIL_USERNAME, // replace with your SMTP username
+        pass: process.env.MAIL_PASSWORD, // replace with your SMTP password
+      },
+    });
+
+    let mailOptions = {
+      from: 'TanatoWebpage <tanato@praxis.ec>', // sender address
+      to: process.env.EMAIL_TO, // list of receivers
+      subject: subject, // Subject line
+      text: message, // plain text body
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent");
   } catch (error) {
-    console.error("Error sending email:", error);
-    throw error;
+    console.error('Error sending email:', error);
   }
 }
