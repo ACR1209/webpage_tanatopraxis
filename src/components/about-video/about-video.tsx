9d457bc2fb60547603e01d99a0c31af4a31dd493
ACR1209 <andrescoronel1209@gmail.com>
2024-06-05 13:48:45 -0500

    remove about video

--- src/components/about-video/about-video.tsx
+++ src/components/about-video/about-video.tsx
@@ -4,10 +4,17 @@ function AboutVideoSection() {
   return (
     <section className="w-full min-h-screen relative px-5 lg:px-44 lg:py-10">
       <h2 className="text-5xl font-bold">Sobre el curso</h2>
-      <video className="w-full h-[500px] lg:h-[700px] lg:mt-5 relative z-10 bg-gray-200" width={1000} height={320} controls>
-        <source src="/videos/about.mp4" type="video/mp4" />
-      </video>
-
+      <iframe
+        className="w-full h-[500px] lg:h-[700px] lg:mt-5 relative z-10 bg-gray-200"
+        width="560"
+        height="715"
+        src="https://www.youtube.com/embed/8HT0JlnY8AA?si=MMNk1VwDRZ-AAMfm"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
       <div className="w-1/2 h-1/5 lg:block hidden absolute bottom-10 right-0  bg-secondary" />
     </section>
   );
