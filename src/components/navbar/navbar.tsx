064e888acbb978e1d188eb05ceedbfb2454884a6
Andrés Alejandro Coronel Rodrigues <78624635+ACR1209@users.noreply.github.com>
2024-06-15 15:58:40 -0500

    put cta in navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { useEffect, useMemo, useState } from "react";
 import SmartNavLink from "./smart-link";
 import { SmartLink } from "@/types/navbar";
+import EnrollButton from "../shared/enroll-button";
 
 function Navbar() {
   const [links, setLinks] = useState<SmartLink[]>([
@@ -49,20 +50,30 @@ function Navbar() {
   }, []);
 
   return (
-    <nav className="flex items-center top-0 z-50 shadow sticky w-full px-14 py-2 bg-white">
-      <Image
-        alt="Logo"
-        src="/shared/PraxisVectorized.png"
-        width={100}
-        height={100}
-      />
-      <ul className="hidden lg:flex gap-5 ml-5">
-        {links.map((link, index) => (
-          <li key={link.name}>
-            <SmartNavLink smartLink={link} />
-          </li>
-        ))}
-      </ul>
+    <nav className="flex items-center top-0 z-50 shadow justify-between sticky w-full px-14 py-2 bg-white">
+      <div className="flex items-center">
+        <Image
+          alt="Logo"
+          src="/shared/PraxisVectorized.png"
+          width={100}
+          height={100}
+        />
+        <ul className="hidden lg:flex gap-5 ml-5">
+          {links.map((link, index) => (
+            <li key={link.name}>
+              <SmartNavLink smartLink={link} />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flex flex-row items-center gap-2">
+        <div className="hidden lg:flex flex-col text-xs mt-2">
+          <p><b>Quito:</b> (+593) 098 773 4581</p>
+          <p><b>Guayaquil:</b> (+593) 096 252 0001</p>
+        </div>
+        <EnrollButton />
+      </div>
     </nav>
   );
 }
