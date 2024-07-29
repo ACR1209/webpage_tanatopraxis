01aefd5c6d6031ce92175a693d35d99e0e8a3bb5
ACR1209 <andrescoronel1209@gmail.com>
2024-07-28 21:35:26 -0500

    added more videos

--- src/components/about-video/about-video.tsx
+++ src/components/about-video/about-video.tsx
@@ -7,17 +7,30 @@ function AboutVideoSection() {
         <b className="text-white bg-primary px-3">Sobre</b>
         <b className="bg-secondary px-3 text-white font-bold">el curso</b>
       </h2>
-      <iframe
-        className="w-full h-[500px] lg:h-[700px] lg:mt-5 relative z-10 bg-gray-200"
-        width="560"
-        height="715"
-        src="https://www.youtube.com/embed/8HT0JlnY8AA?si=MMNk1VwDRZ-AAMfm"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+
+      <video className="w-full h-[500px] lg:h-[700px] lg:mt-5 relative z-10 bg-gray-200"
+        width={1000}
+        height={320}
+        controls
+
+      >
+        <source src="/videos/videoTanato.mp4" type="video/mp4" />
+      </video>
+
+      <div className="w-full h-[500px] lg:h-[700px] lg:mt-5 relative z-10  flex">
+        <video controls className="w-1/3 px-5 h-full">
+          <source src="/videos/oecVideo.mp4" type="video/mp4" />
+        </video>
+        <video controls className="w-1/3 px-5 h-full">
+          <source src="/videos/talleresVideo.mp4" type="video/mp4" />
+        </video>
+        <video controls className="w-1/3 px-5 h-full">
+          <source src="/videos/idkVideo.mp4" type="video/mp4" />
+        </video>
+
+      </div>
+
+
       <div className="w-1/2 h-1/5 lg:block hidden absolute bottom-10 right-0  bg-secondary" />
     </section>
   );
