d91493c24d0cdda8a42c3b4f0471eb745bb23b87
ACR1209 <andrescoronel1209@gmail.com>
2024-08-22 13:46:22 -0500

    fix endorsed in mobile

--- src/components/endorsed-section/endorsed-oec-section.tsx
+++ src/components/endorsed-section/endorsed-oec-section.tsx
@@ -6,20 +6,20 @@ function EndorsedOECSection() {
         <div className='flex flex-col items-center mt-10'>
 
             <h3 className='text-3xl font-bold text-primary'>El OEC está avalado por</h3>
-            <div className='flex flex-row w-full justify-center gap-10 items-center mt-3'>
+            <div className='flex flex-col mb-10 lg:flex-row w-full justify-center gap-10 items-center mt-3'>
                 <Image
                     alt="logo de la universidad de los hemisferios"
                     src="/logos/ministeriotrabajo.png"
                     width={1080}
                     height={1080}
-                    className="h-40 w-auto"
+                    className="h-24 lg:h-40 w-auto"
                 />
                 <Image
                     alt="logo de la cámara artesanal del pichincha"
                     src="/logos/senescyt.png"
                     width={1080}
                     height={1080}
-                    className="h-24 w-auto"
+                    className="h-12 lg:h-24 w-auto"
                 />
             </div>
         </div>
--- src/components/endorsed-section/endorsed-section.tsx
+++ src/components/endorsed-section/endorsed-section.tsx
@@ -6,20 +6,20 @@ function EndorsedSection() {
         <div className='flex flex-col items-center mt-10'>
 
             <h3 className='text-3xl font-bold text-primary'>Estamos avalados por</h3>
-            <div className='flex flex-row w-full justify-center gap-5 items-center mt-3'>
+            <div className='flex flex-col lg:flex-row w-full justify-center gap-5 items-center mt-3'>
                 <Image
                     alt="logo de la universidad de los hemisferios"
                     src="/logos/hemisferios.png"
                     width={1080}
                     height={1080}
-                    className="h-24 w-auto"
+                    className="h-12 lg:h-24 w-auto"
                 />
                 <Image
                     alt="logo de la cámara artesanal del pichincha"
                     src="/logos/cap.png"
                     width={1080}
                     height={1080}
-                    className="h-40 w-auto"
+                    className="h-24 lg:h-40 w-auto"
                 />
             </div>
         </div>
