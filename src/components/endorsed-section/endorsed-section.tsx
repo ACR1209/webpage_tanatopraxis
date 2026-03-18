03a9c91f28b7b8378798f929eb1c55934e94c534
Andrés Alejandro Coronel Rodrigues <78624635+ACR1209@users.noreply.github.com>
2026-03-18 15:20:11 -0500

    Remove university logo from endorsed section
    Removed university logo from the endorsed section.

--- src/components/endorsed-section/endorsed-section.tsx
+++ src/components/endorsed-section/endorsed-section.tsx
@@ -8,13 +8,6 @@ function EndorsedSection() {
             <h3 className='text-3xl font-bold text-primary'>Estamos avalados por</h3>
             <div className='flex flex-col lg:flex-row w-full justify-center gap-5 items-center mt-3'>
                 <Image
-                    alt="logo de la universidad de los hemisferios"
-                    src="/logos/hemisferios.png"
-                    width={1080}
-                    height={1080}
-                    className="h-12 lg:h-24 w-auto"
-                />
-                <Image
                     alt="logo de la cámara artesanal del pichincha"
                     src="/logos/cap.png"
                     width={1080}
